49adb1d10d5f5faa553efed9fcd7799ea1f289c1
whatknows <jedbrubaker@gmail.com>
2023-07-06 15:28:30 +1000

    Added mkdir blocks

--- prepareCorpus/collect.py
+++ prepareCorpus/collect.py
@@ -6,6 +6,14 @@ import os
 index_id = 'CC-MAIN-2022-05'
 index_url = 'https://data.commoncrawl.org/crawl-data/'+index_id+'/cc-index.paths.gz'
 
+# check to see if index_files folder exists
+if os.path.exists('../index_files'):
+    pass
+else:
+    os.mkdir('../index_files')
+
+# download data files
+# TODO: check if files already exist
 req = requests.get(index_url)
 with open('../index_files/'+index_id+'.gz', 'wb') as f:
     f.write(req.content)
@@ -13,18 +21,25 @@ with gzip.open('../index_files/'+index_id+'.gz', 'rb') as f_in:
     with open('../index_files/'+index_id+'.txt', 'wb') as f_out:
         shutil.copyfileobj(f_in, f_out)
 
-
-
 with open('../index_files/'+index_id+'.txt', 'r') as fin:
     cdx_files = fin.readlines()
 cdx_id = cdx_files[0].strip()
 cdx_url = 'https://data.commoncrawl.org/'+cdx_id
 cdx_fname = cdx_id.split('/')[-1].split('.')[0]
 req = requests.get(cdx_url)
+
+# check to see if cdx_files folder exists
+if os.path.exists('../cdx_files'):
+    pass
+else:
+    os.mkdir('../cdx_files')
+
+# check and see if cdx data files exist
 if os.path.exists('../cdx_files/'+index_id):
     pass
 else:
     os.mkdir('../cdx_files/'+index_id)
+    
 with open('../cdx_files/'+index_id+'/'+cdx_fname, 'wb') as f:
     f.write(req.content)
 with gzip.open('../cdx_files/'+index_id+'/'+cdx_fname, 'rb') as f_in:
